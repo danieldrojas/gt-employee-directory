081b315124dab1b151ab1e3cd4b647182d36d68c
Daniel Rojas <daniel.drojas@outlook.com>
2020-08-03 22:10:42 -0400

    Updates

---
+++ precache-manifest.93f84fc0be269c89d46ad8c26c50d4fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d0964eb492cad4cdee6dd28e8eb43e",
+    "url": "/gt-employee-directory/index.html"
+  },
+  {
+    "revision": "dfff95c3328c0f8065f4",
+    "url": "/gt-employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e78395595c83d339bad8",
+    "url": "/gt-employee-directory/static/js/2.45e26e31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gt-employee-directory/static/js/2.45e26e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfff95c3328c0f8065f4",
+    "url": "/gt-employee-directory/static/js/main.8be1d754.chunk.js"
+  },
+  {
+    "revision": "1bf594e7cd191fab01b6",
+    "url": "/gt-employee-directory/static/js/runtime-main.4bce744e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gt-employee-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
